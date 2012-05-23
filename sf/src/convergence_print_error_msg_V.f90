60e9fca0e7074585fdd59c9d0f9f9746e33c53f4
Adriano Amaricci <adriano.amaricci@gmail.com>
2012-05-23 14:40:20 +0200

    Updated TOOLS/check_convergence to include new possible checks: I,D,Z - scalars,functions_1dim. Tested in RDMFT to check over the order parameter of the lattice.

--- sf/src/convergence_print_error_msg_V.f90
+++
@@ -1,9 +0,0 @@
-  if(convergence)then
-     write(*,"(A,F18.12)")bold_green("error="),err
-  else
-     if(err < eps)then
-        write(*,"(A,F18.12)")bold_yellow("error="),err
-     else
-        write(*,"(A,F18.12)")bold_red("error="),err
-     endif
-  endif
