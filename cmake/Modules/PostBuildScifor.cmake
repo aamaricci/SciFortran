fd17bbd8b4aa7b311f254d31e73fceb5d46dfcab
Adriano Amaricci <adriano.amaricci@gmail.com>
2017-05-08 11:14:49 +0200

    v4.1.1 UPDATE
    Few fixes to the new version CMakefile.txt:
    - added *pkg-config* configuration file as an extra usage option.
    The .pc file containing all information about the installed
    library, is placed in the installation directory in etc.
    It is pre-pended to the env variable PKG_CONFIG_PATH with the
    environment module or within configvars.sh script. Otherwise
    it would be enough to add the target directory containing the .pc
    file to this env variable:
    export PKG_CONFIG_PATH=path-to-dir-containing-pc-file
    
    - fixed an issue with the Scifor Versions module
    
    - CMakefile provides useful after-installation informations.

--- cmake/Modules/PostBuildScifor.cmake
+++ cmake/Modules/PostBuildScifor.cmake
@@ -1,6 +1,7 @@
-FUNCTION(BUILD_SF_ENV_MODULE FILE)
-  MESSAGE("-+ Writing tmp env. module to ${FILE}")
+FUNCTION(BUILD_ENV_MODULE FILE)
+  MESSAGE(STATUS "${BoldYellow}Writing tmp env. module to ${FILE}${ColourReset}")
   FILE(WRITE  ${FILE}  "#%Modules\n")
+  FILE(APPEND ${FILE} "set project ${PROJECT_NAME}\n")
   FILE(APPEND ${FILE} "set root ${PREFIX}\n")
   FILE(APPEND ${FILE} "set plat ${FC_PLAT}\n")
   FILE(APPEND ${FILE} "set version \"${VERSION} (${FC_PLAT})\"\n")
@@ -11,8 +12,18 @@ ENDFUNCTION()
 
 
 
-FUNCTION(BUILD_SF_CONFIGVARS FILE)
+FUNCTION(BUILD_CONFIGVARS FILE)
+  MESSAGE(STATUS "${BoldYellow}Setup configvars.sh${ColourReset}")
   FILE(READ   ${SF_BIN}/configvars.sh CONTENTS)
   FILE(WRITE  ${FILE} "${CONTENTS}")
   FILE(APPEND ${FILE} "add_library_to_system ${CMAKE_INSTALL_PREFIX}\n")
+ENDFUNCTION()
+
+
+FUNCTION(BUILD_PKCONFIG FILE)
+  MESSAGE(STATUS "${BoldYellow}Writing tmp pk-config file to ${FILE}${ColourReset}")
+  FILE(WRITE ${FILE} "prefix=${CMAKE_INSTALL_PREFIX}\n")
+  FILE(READ   ${SF_ETC}/${PROJECT_NAME}.pc CONTENTS)
+  FILE(APPEND ${FILE} "${CONTENTS}")
+  FILE(APPEND ${FILE} "Version:${VERSION}\n")
 ENDFUNCTION()
