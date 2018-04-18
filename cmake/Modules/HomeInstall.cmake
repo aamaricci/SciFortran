003eea67b976d0ce517cf74bc3c7ec7fd91bb356
Adriano Amaricci <adriano.amaricci@gmail.com>
2018-04-18 09:55:28 +0200

    v4.3.2 BUG FIX
    Bug fixed in CMakeLists.txt and cmake/modules/homeinstall, producing an erroneous
    post-installation message.

--- cmake/Modules/HomeInstall.cmake
+++ cmake/Modules/HomeInstall.cmake
@@ -1,6 +1,7 @@
 SET(CMAKE_SOURCE_DIR ${CMAKE_ARGV3})
 SET(CMAKE_INSTALL_PREFIX ${CMAKE_ARGV4})
 SET(PROJECT_NAME ${CMAKE_ARGV5})
+SET(FC_PLAT ${CMAKE_ARGV6})
 
 SET(CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/cmake/Modules")
 SET(SF_TARGET_LIB ${CMAKE_INSTALL_PREFIX}/lib)
@@ -22,20 +23,23 @@ FILE(INSTALL ${PKCONFIG_FILE} DESTINATION $ENV{HOME}/.pkgconfig.d
 MESSAGE( STATUS "After installation usage options:")
 MESSAGE( STATUS
   "
-${Red} GLOBAL INSTALLATION${ColourReset}:
-${Yellow} Add this line to the system shell configuration file (i.e. /etc/bash.bashrc)${ColourReset}:
-   source ${DT_TARGET_BIN}/scifor_config_global.sh
+>>${Red}GLOBAL INSTALLATION${ColourReset}:
+${Yellow}(if you are admin of a multi-user machine and you want users to automatically access the library)${ColourReset}
+${Green} Add this line to the system shell configuration file (i.e. /etc/bash.bashrc)${ColourReset}:
+   $ source ${SF_TARGET_BIN}/scifor_config_global.sh
 
-${Red} USER INSTALLATION${ColourReset}:
+
+>>${Red}USER INSTALLATION${ColourReset}:
+${Yellow}(if you are regular user of a machine, any of the followings will give you access to the library)${ColourReset}
 Pick ONE choice or add in your bash profile (i.e. ~/.bashrc):
-${Yellow}A. source the config script: ${SF_TARGET_BIN}/dmft_tools_config_user.sh${ColourReset}:
-   source ${DT_TARGET_BIN}/scifor_config_user.sh
+${Green}A. source the config script: ${SF_TARGET_BIN}/scifor_config_user.sh, i.e.${ColourReset}:
+   $ source ${SF_TARGET_BIN}/scifor_config_user.sh
 
-${Yellow}B. use the provided ${PROJECT_NAME} environment module in ${SF_TARGET_ETC}${ColourReset}:
-   module use $HOME/.modules.d
-   module load ${PROJECT_NAME}/${FC_PLAT}
+${Green}B. use the provided ${PROJECT_NAME} environment module in ${SF_TARGET_ETC}, i.e.${ColourReset}:
+   $ module use $HOME/.modules.d
+   $ module load ${PROJECT_NAME}/${FC_PLAT}
 
-${Yellow}C. use pkg-config with the provided ${PROJECT_NAME}.pc in ${SF_TARGET_ETC}${ColourReset}:
-   export PKG_CONFIG_PATH=${DT_TARGET_ETC}/:$PKG_CONFIG_PATH
-   pkg-config --cflags --libs ${PROJECT_NAME} (to get lib info)
+${Green}C. use pkg-config with the provided ${PROJECT_NAME}.pc in ${SF_TARGET_ETC}, i.e.${ColourReset}:
+   $ export PKG_CONFIG_PATH=${SF_TARGET_ETC}/:$PKG_CONFIG_PATH
+   $ pkg-config --cflags --libs ${PROJECT_NAME} (to get lib info)
 ")
