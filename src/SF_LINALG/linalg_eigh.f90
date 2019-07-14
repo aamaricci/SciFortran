c950fd751f8ac4f2aa47a0ca844ab9fe635d0549
Adriano Amaricci <adriano.amaricci@gmail.com>
2019-07-14 10:25:48 +0200

    Intermediate commit:
    - Updated +eigh interface to use multiple methods. Default remains dsyev/zheev-r.
    - Using UPLO='L' rather then 'U' [we will see later why this is necessary to have
    compatibility with Scalapack]

--- src/SF_LINALG/linalg_eigh.f90
+++ src/SF_LINALG/linalg_eigh.f90
@@ -82,35 +82,42 @@ end subroutine zeigh_generalized
 
 
 
-subroutine deigh_simple(A,W,jobz,uplo,vl,vu,il,iu,tol)
+
+
+
+
+subroutine deigh_simple(A,E,method,jobz,uplo,vl,vu,il,iu,tol)
   real(8),dimension(:,:),intent(inout)       :: A ! M v = E v/v(i,j) = ith component of jth vec.
-  real(8),dimension(size(A,2)),intent(inout) :: W ! eigenvalues
+  real(8),dimension(size(A,2)),intent(inout) :: E ! eigenvalues
+  character(len=*),optional                  :: method
   character(len=1),optional                  :: jobz,uplo
   character(len=1)                           :: jobz_,uplo_,range
+  character(len=20)                          :: method_
   real(8),optional                           :: vl,vu,tol
   integer,optional                           :: il,iu
   real(8)                                    :: vL_,vU_,tol_
   integer                                    :: iL_,iU_
-  integer                                    :: i,j,n,lda,info,ldz
-  integer                                    :: lwork,liwork,m
-  integer                                    :: guess_iwork(1)
-  real(8)                                    :: guess_work(1)
+  integer                                    :: Ns
+  integer                                    :: i,j,info    
+  integer                                    :: lwork,liwork,mE
+  integer                                    :: guess_liwork(1)
+  real(8)                                    :: guess_lwork(1)
   logical                                    :: boolV,boolI
   real(8),dimension(:,:),allocatable         :: Z
-  real(8),dimension(:),allocatable           :: work
-  integer,dimension(:),allocatable           :: Isuppz
+  real(8),dimension(:),allocatable           :: work,rwork
+  integer,dimension(:),allocatable           :: Isuppz,Ifail
   integer,dimension(:),allocatable           :: Iwork
   !
-  !
+  method_='dsyevr';if(present(method))method_=trim(method)
   jobz_='V'  ;if(present(jobz))jobz_=jobz
-  uplo_='U'  ;if(present(uplo))uplo_=uplo
+  uplo_='L'  ;if(present(uplo))uplo_=uplo
   vl_  = 1d0 ;if(present(vL))vL_=vL
   vu_  = 1d0 ;if(present(vU))vU_=vU
   iL_  = 1   ;if(present(iL))iL_=iL
   iU_  = 1   ;if(present(iU))iU_=iU
   tol_ = 0d0 ;if(present(tol))tol_=tol
   !
-  W=0d0
+  E=0d0
   !
   range='A'
   boolV=present(vL).AND.present(vU)
@@ -119,59 +126,86 @@ subroutine deigh_simple(A,W,jobz,uplo,vl,vu,il,iu,tol)
   if(boolV)range='V'
   if(boolI)range='I'
   !
-  N     = max(1,size(A,1))
-  LDA   = max(1,size(A,2))
-  ! M     = N ; if(range=="I")M = iU_ - iL_ + 1
-  LDZ   = N
-  if(any(shape(A)/=[N,N]))stop "my_eighD error: A has illegal shape"
+  Ns = max(1,size(A,1))
+  if(any(shape(A)/=[Ns,Ns]))stop "my_eighD error: A has illegal shape"
   !
-  allocate(Z(N,N)) !Supplying N columns is always safe.
-  !
-  allocate(Isuppz(2*N))
-  !
-  call dsyevr(jobz_,range,uplo_,N,A,N,vl_,vu_,iL_,iU_,tol_,M,W,Z,N,Isuppz,guess_work,-1,guess_iwork,-1,info)
-  lwork = guess_work(1)
-  liwork= guess_iwork(1)
-  allocate(work(lwork))
-  allocate(iwork(liwork))
-  call dsyevr(jobz_,range,uplo_,N,A,N,vl_,vu_,iL_,iU_,tol_,M,W,Z,N,Isuppz,work,lwork,iwork,liwork,info)
-  !<copy the Evecs from Z to the input matrix A
-  !print*,range,M
-  A = 0d0
-  A(:,1:M) = Z(:,1:M)    
+  select case(method_)
+  case default
+     allocate(Isuppz(2*Ns))
+     allocate(Z(Ns,Ns))
+     call dsyevr(jobz_,range,uplo_,Ns,A,Ns,vl_,vu_,iL_,iU_,tol_,mE,E,Z,Ns,Isuppz,guess_lwork,-1,guess_liwork,-1,info)
+     lwork = guess_lwork(1)
+     liwork= guess_liwork(1)
+     allocate(work(lwork))
+     allocate(iwork(liwork))
+     call dsyevr(jobz_,range,uplo_,Ns,A,Ns,vl_,vu_,iL_,iU_,tol_,mE,E,Z,Ns,Isuppz,work,lwork,iwork,liwork,info)
+     if(jobz_=='V') A(:,1:mE) = Z(:,1:mE)
+     !
+  case ("dsyev")
+     call dsyev(jobz_,uplo_,Ns,A,Ns,E,guess_lwork,-1,info)
+     lwork = guess_lwork(1)
+     allocate(work(lwork))
+     call dsyev(jobz_,uplo_,Ns,A,Ns,E,work,lwork,info)
+     !
+  case("dsyevd")
+     call dsyevd(jobz_,uplo_,Ns,A,Ns,E,guess_lwork,-1,guess_liwork,-1,info)
+     lwork = guess_lwork(1)
+     liwork= guess_liwork(1)
+     allocate(work(lwork))
+     allocate(iwork(liwork))
+     call dsyevd(jobz_,uplo_,Ns,A,Ns,E,work,lwork,iwork,liwork,info)
+     !
+  case ("dsyevx")
+     allocate(Z(Ns,Ns))
+     allocate(Ifail(Ns))
+     allocate(rwork(7*Ns))
+     allocate(iwork(5*Ns))
+     call dsyevx(jobz_,range,uplo_,Ns,A,Ns,vl_,vu_,iL_,iU_,tol_,mE,E,Z,Ns,guess_lwork,-1,rwork,iwork,ifail,info)
+     lwork = guess_lwork(1)
+     allocate(work(lwork))
+     call dsyevx(jobz_,range,uplo_,Ns,A,Ns,vl_,vu_,iL_,iU_,tol_,mE,E,Z,Ns,work,lwork,rwork,iwork,ifail,info)
+     if(jobz_=='V') A(:,1:mE) = Z(:,1:mE)
+  end select
+  return
 end subroutine deigh_simple
 
-subroutine zeigh_simple(A,W,jobz,uplo,vl,vu,il,iu,tol)
-  complex(8),dimension(:,:),intent(inout)    :: A ! M v = E v/v(i,j) = ith component of jth vec.
-  real(8),dimension(size(A,2)),intent(inout) :: W ! eigenvalues
+
+subroutine zeigh_simple(A,E,method,jobz,uplo,vl,vu,il,iu,tol)
+  complex(8),dimension(:,:),intent(inout)       :: A ! M v = E v/v(i,j) = ith cmpt of jth vec.
+  real(8),dimension(size(A,2)),intent(inout) :: E ! eigenvalues
+  character(len=*),optional                  :: method
   character(len=1),optional                  :: jobz,uplo
   character(len=1)                           :: jobz_,uplo_,range
+  character(len=20)                          :: method_
   real(8),optional                           :: vl,vu,tol
   integer,optional                           :: il,iu
   real(8)                                    :: vL_,vU_,tol_
   integer                                    :: iL_,iU_
-  integer                                    :: i,j,n,lda,info,ldz
-  integer                                    :: lwork,liwork,lrwork,m
-  integer                                    :: guess_iwork(1)
-  real(8)                                    :: guess_rwork(1)
-  complex(8)                                 :: guess_work(1)
+  integer                                    :: Ns
+  integer                                    :: i,j,info    
+  integer                                    :: lwork,liwork,lrwork,mE
+  complex(8)                                 :: guess_lwork(1)
+  real(8)                                    :: guess_lrwork(1)
+  integer                                    :: guess_liwork(1)
   logical                                    :: boolV,boolI
   complex(8),dimension(:,:),allocatable      :: Z
-  complex(8),dimension(:),allocatable        :: work
-  complex(8),dimension(:),allocatable        :: rwork
-  integer,dimension(:),allocatable           :: Isuppz
+  complex(8),dimension(:),allocatable        :: Work
+  real(8),dimension(:),allocatable           :: Rwork
   integer,dimension(:),allocatable           :: Iwork
+  integer,dimension(:),allocatable           :: Isuppz
+  integer,dimension(:),allocatable           :: Ifail
+  real(8),external :: dlamch
   !
-  !
+  method_='zheevr';if(present(method))method_=trim(method)
   jobz_='V'  ;if(present(jobz))jobz_=jobz
-  uplo_='U'  ;if(present(uplo))uplo_=uplo
+  uplo_='L'  ;if(present(uplo))uplo_=uplo
   vl_  = 1d0 ;if(present(vL))vL_=vL
   vu_  = 1d0 ;if(present(vU))vU_=vU
   iL_  = 1   ;if(present(iL))iL_=iL
   iU_  = 1   ;if(present(iU))iU_=iU
-  tol_ = 0d0 ;if(present(tol))tol_=tol
+  tol_ = dlamch('s') ;if(present(tol))tol_=tol
   !
-  W=0d0
+  E=0d0
   !
   range='A'
   boolV=present(vL).AND.present(vU)
@@ -180,28 +214,189 @@ subroutine zeigh_simple(A,W,jobz,uplo,vl,vu,il,iu,tol)
   if(boolV)range='V'
   if(boolI)range='I'
   !
-  N     = max(1,size(A,1))
-  LDA   = max(1,size(A,2))
-  ! M     = N ; if(range=="I")M = iU_ - iL_ + 1
-  LDZ   = N
-  if(any(shape(A)/=[N,N]))stop "my_eighD error: A has illegal shape"
+  Ns = max(1,size(A,1))
+  if(any(shape(A)/=[Ns,Ns]))stop "my_eighD error: A has illegal shape"
   !
-  allocate(Z(N,N)) !Supplying N columns is always safe.
-  !
-  allocate(Isuppz(2*N))
-  !
-  call zheevr(jobz_,range,uplo_,N,A,N,vl_,vu_,iL_,iU_,tol_,M,W,Z,N,Isuppz,guess_work,-1,guess_rwork,-1,guess_iwork,-1,info)
-  lwork = guess_work(1)
-  lrwork= guess_rwork(1)
-  liwork= guess_iwork(1)
-  allocate(work(lwork))
-  allocate(rwork(lrwork))
-  allocate(iwork(liwork))
-  call zheevr(jobz_,range,uplo_,N,A,N,vl_,vu_,iL_,iU_,tol_,M,W,Z,N,Isuppz,work,lwork,rwork,lrwork,iwork,liwork,info)
-  !<copy the Evecs from Z to the input matrix A
-  A = dcmplx(0d0,0d0)
-  A(:,1:M) = Z(:,1:M)    
+  mE = Ns
+  select case(method_)
+  case default
+     allocate(Isuppz(2*Ns))
+     allocate(Z(Ns,Ns))
+     call zheevr(jobz_,range,uplo_,&
+          Ns,A,Ns,&
+          vl_,vu_,iL_,iU_,tol_,&
+          ME,E,Z,Ns,&
+          Isuppz,guess_lwork,-1,guess_lrwork,-1,guess_liwork,-1,info)
+     lwork = guess_lwork(1)
+     lrwork= guess_lrwork(1)
+     liwork= guess_liwork(1)
+     allocate(work(lwork))
+     allocate(rwork(lrwork))
+     allocate(iwork(liwork))
+     call zheevr(jobz_,range,uplo_,&
+          Ns,A,Ns,&
+          vl_,vu_,iL_,iU_,tol_,&
+          ME,E,Z,Ns,&
+          Isuppz,work,lwork,rwork,lrwork,iwork,liwork,info)
+     !<copy the Evecs from Z to the input matrix A
+     if(jobz_=='V') A(:,1:mE) = Z(:,1:mE)    
+     !
+  case ("zheev")
+     allocate(rwork(max(1,3*Ns)))
+     call zheev(jobz_,uplo_,Ns,A,Ns,E,guess_lwork,-1,rwork,info)
+     lwork = guess_lwork(1)
+     allocate(work(lwork))
+     call zheev(jobz_,uplo_,Ns,A,Ns,E,work,lwork,rwork,info)
+     !
+  case("zheevd")
+     call zheevd(jobz_,uplo_,Ns,A,Ns,E,guess_lwork,-1,guess_lrwork,-1,guess_liwork,-1,info)
+     lwork = guess_lwork(1) ; lrwork= guess_lrwork(1) ; liwork= guess_liwork(1)
+     allocate(work(lwork))
+     allocate(rwork(lrwork))
+     allocate(iwork(liwork))
+     call zheevd(jobz_,uplo_,Ns,A,Ns,E,work,lwork,rwork,lrwork,iwork,liwork,info)
+     !
+  case ("zheevx")
+     allocate(Z(Ns,Ns))
+     allocate(Ifail(Ns))
+     allocate(rwork(7*Ns))
+     allocate(iwork(5*Ns))
+     call zheevx(jobz_,range,uplo_,Ns,A,Ns,&
+          vl_,vu_,iL_,iU_,tol_,mE,E,Z,Ns,guess_lwork,-1,rwork,iwork,ifail,info)
+     lwork = guess_lwork(1)
+     allocate(work(lwork))
+     call zheevx(jobz_,range,uplo_,Ns,A,Ns,&
+          vl_,vu_,iL_,iU_,tol_,mE,E,Z,Ns,work,lwork,rwork,iwork,ifail,info)
+     if(jobz_=='V') A(:,1:mE) = Z(:,1:mE)
+  end select
+  return
 end subroutine zeigh_simple
+
+
+
+
+
+
+! subroutine deigh_simple(A,W,jobz,uplo,vl,vu,il,iu,tol)
+!   real(8),dimension(:,:),intent(inout)       :: A ! M v = E v/v(i,j) = ith component of jth vec.
+!   real(8),dimension(size(A,2)),intent(inout) :: W ! eigenvalues
+!   character(len=1),optional                  :: jobz,uplo
+!   character(len=1)                           :: jobz_,uplo_,range
+!   real(8),optional                           :: vl,vu,tol
+!   integer,optional                           :: il,iu
+!   real(8)                                    :: vL_,vU_,tol_
+!   integer                                    :: iL_,iU_
+!   integer                                    :: i,j,n,lda,info,ldz
+!   integer                                    :: lwork,liwork,m
+!   integer                                    :: guess_iwork(1)
+!   real(8)                                    :: guess_work(1)
+!   logical                                    :: boolV,boolI
+!   real(8),dimension(:,:),allocatable         :: Z
+!   real(8),dimension(:),allocatable           :: work
+!   integer,dimension(:),allocatable           :: Isuppz
+!   integer,dimension(:),allocatable           :: Iwork
+!   !
+!   !
+!   jobz_='V'  ;if(present(jobz))jobz_=jobz
+!   uplo_='U'  ;if(present(uplo))uplo_=uplo
+!   vl_  = 1d0 ;if(present(vL))vL_=vL
+!   vu_  = 1d0 ;if(present(vU))vU_=vU
+!   iL_  = 1   ;if(present(iL))iL_=iL
+!   iU_  = 1   ;if(present(iU))iU_=iU
+!   tol_ = 0d0 ;if(present(tol))tol_=tol
+!   !
+!   W=0d0
+!   !
+!   range='A'
+!   boolV=present(vL).AND.present(vU)
+!   boolI=present(iL).OR.present(iU)
+!   if(boolV.and.boolI)stop "vX and iX arguments both present. Can not set RANGE"
+!   if(boolV)range='V'
+!   if(boolI)range='I'
+!   !
+!   N     = max(1,size(A,1))
+!   LDA   = max(1,size(A,2))
+!   ! M     = N ; if(range=="I")M = iU_ - iL_ + 1
+!   LDZ   = N
+!   if(any(shape(A)/=[N,N]))stop "my_eighD error: A has illegal shape"
+!   !
+!   allocate(Z(N,N)) !Supplying N columns is always safe.
+!   !
+!   allocate(Isuppz(2*N))
+!   !
+!   call dsyevr(jobz_,range,uplo_,N,A,N,vl_,vu_,iL_,iU_,tol_,M,W,Z,N,Isuppz,guess_work,-1,guess_iwork,-1,info)
+!   lwork = guess_work(1)
+!   liwork= guess_iwork(1)
+!   allocate(work(lwork))
+!   allocate(iwork(liwork))
+!   call dsyevr(jobz_,range,uplo_,N,A,N,vl_,vu_,iL_,iU_,tol_,M,W,Z,N,Isuppz,work,lwork,iwork,liwork,info)
+!   !<copy the Evecs from Z to the input matrix A
+!   !print*,range,M
+!   A = 0d0
+!   A(:,1:M) = Z(:,1:M)    
+! end subroutine deigh_simple
+
+! subroutine zeigh_simple(A,W,jobz,uplo,vl,vu,il,iu,tol)
+!   complex(8),dimension(:,:),intent(inout)    :: A ! M v = E v/v(i,j) = ith component of jth vec.
+!   real(8),dimension(size(A,2)),intent(inout) :: W ! eigenvalues
+!   character(len=1),optional                  :: jobz,uplo
+!   character(len=1)                           :: jobz_,uplo_,range
+!   real(8),optional                           :: vl,vu,tol
+!   integer,optional                           :: il,iu
+!   real(8)                                    :: vL_,vU_,tol_
+!   integer                                    :: iL_,iU_
+!   integer                                    :: i,j,n,lda,info,ldz
+!   integer                                    :: lwork,liwork,lrwork,m
+!   integer                                    :: guess_iwork(1)
+!   real(8)                                    :: guess_rwork(1)
+!   complex(8)                                 :: guess_work(1)
+!   logical                                    :: boolV,boolI
+!   complex(8),dimension(:,:),allocatable      :: Z
+!   complex(8),dimension(:),allocatable        :: work
+!   complex(8),dimension(:),allocatable        :: rwork
+!   integer,dimension(:),allocatable           :: Isuppz
+!   integer,dimension(:),allocatable           :: Iwork
+!   !
+!   !
+!   jobz_='V'  ;if(present(jobz))jobz_=jobz
+!   uplo_='U'  ;if(present(uplo))uplo_=uplo
+!   vl_  = 1d0 ;if(present(vL))vL_=vL
+!   vu_  = 1d0 ;if(present(vU))vU_=vU
+!   iL_  = 1   ;if(present(iL))iL_=iL
+!   iU_  = 1   ;if(present(iU))iU_=iU
+!   tol_ = 0d0 ;if(present(tol))tol_=tol
+!   !
+!   W=0d0
+!   !
+!   range='A'
+!   boolV=present(vL).AND.present(vU)
+!   boolI=present(iL).OR.present(iU)
+!   if(boolV.and.boolI)stop "vX and iX arguments both present. Can not set RANGE"
+!   if(boolV)range='V'
+!   if(boolI)range='I'
+!   !
+!   N     = max(1,size(A,1))
+!   LDA   = max(1,size(A,2))
+!   ! M     = N ; if(range=="I")M = iU_ - iL_ + 1
+!   LDZ   = N
+!   if(any(shape(A)/=[N,N]))stop "my_eighD error: A has illegal shape"
+!   !
+!   allocate(Z(N,N)) !Supplying N columns is always safe.
+!   !
+!   allocate(Isuppz(2*N))
+!   !
+!   call zheevr(jobz_,range,uplo_,N,A,N,vl_,vu_,iL_,iU_,tol_,M,W,Z,N,Isuppz,guess_work,-1,guess_rwork,-1,guess_iwork,-1,info)
+!   lwork = guess_work(1)
+!   lrwork= guess_rwork(1)
+!   liwork= guess_iwork(1)
+!   allocate(work(lwork))
+!   allocate(rwork(lrwork))
+!   allocate(iwork(liwork))
+!   call zheevr(jobz_,range,uplo_,N,A,N,vl_,vu_,iL_,iU_,tol_,M,W,Z,N,Isuppz,work,lwork,rwork,lrwork,iwork,liwork,info)
+!   !<copy the Evecs from Z to the input matrix A
+!   A = dcmplx(0d0,0d0)
+!   A(:,1:M) = Z(:,1:M)    
+! end subroutine zeigh_simple
 
 
 ! subroutine deigh_simple(M,E,jobz,uplo)
